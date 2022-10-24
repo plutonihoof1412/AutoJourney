f0bf72f091eee3e9033cdfb3d96ee4607fcac8e0
Phuong Uyen <plutonihoof1412@gmail.com>
2022-10-24 09:17:18 +0700

    Rename classes

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -25,28 +25,22 @@ public class Topic_02_Selenium_Locator {
 		
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
+
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
+	public void TC_01_() {
+		
 	}
 
 	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+	public void TC_02_() {
+		
 	}
 
 	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+	public void TC_03_() {
+		
 	}
 
 	@AfterClass
